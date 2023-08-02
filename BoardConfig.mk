87dc833c7f49625a9234d818d800e13b0eef9ef9
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-08-02 19:17:38 +0530

    hawao: Adapt for EvolutionX

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ DEVICE_PATH := device/motorola/hawao
 TARGET_BOOTLOADER_BOARD_NAME := hawao
 
 # Display
-TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_DENSITY := 450
 
 # HIDL
 ODM_MANIFEST_SKUS += b bc d dc dn dnc n nc
